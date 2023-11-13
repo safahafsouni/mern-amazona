c82e508e15e25991cef526020d2cc1756180e7f2
safahafsouni <safa.hafsouni@gmail.com>
2023-11-13 21:20:17 +0100

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Amazona</a>
+      </header>
+      <main>
+        List products
+      </main>
+    </div>
+  );
+}
+
+export default App;
